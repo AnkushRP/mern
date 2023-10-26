a969f8c12aca350aa9ceb30c6efb0f8f8d7e1427
AnkushRP <147685188+AnkushRP@users.noreply.github.com>
2023-10-26 20:20:16 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+//console.log("Hello Everyone");
+const express = require("express")
+const mongoose =require("mongoose");
+const app=express()
+//Db connection
+mongoose.connect("mongodb+srv://ankushrp2004:ankush123@cluster0.nvbesvi.mongodb.net/internship-batch3").then(()=>{
+    console.log("Database is connected successfully");
+})
+.catch((err)=>{
+    console.log(err,"something went wrong");
+})
+//Test Api
+app.get("/test",(req,res)=>{
+    res.send("Hello Everyone, This is Test Api ");
+});
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000");
+});
